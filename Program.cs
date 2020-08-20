f10490c52b2cef672fb0915b611bdd318b63301f
BrunoMonte <65718668+BrunoMonte@users.noreply.github.com>
2020-08-20 10:37:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroVezes = 5;
+
+            for (int i = 0; i < numeroVezes; i++)
+            {
+                Console.WriteLine($"Velozes e Furiosos {i}");    //write.line, coloca tudo em uma linha
+            }
+        }
+    }
+}
+
